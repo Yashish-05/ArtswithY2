4d4de82f17e7e83fdf747cf28667a643461e358f
Yashish <yashishkumarkamboj@gmail.com>
2026-08-06 18:38:32 +0530

    Use SMTP 587

--- Backend/services/email.service.js
+++ Backend/services/email.service.js
@@ -72,11 +72,14 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false, // IMPORTANT
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
     },
 });
 
