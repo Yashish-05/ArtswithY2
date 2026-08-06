c4cfab29e13c71de3f07efdd36857355b503d962
Yashish <yashishkumarkamboj@gmail.com>
2026-08-06 14:52:44 +0530

    Configure CORS for Vercel

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://artswith-y2.vercel.app",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
